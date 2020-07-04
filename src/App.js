88630defcdec25b4ba4263bebc0be97e9178a74b
alexbol99 <alexbol99@gmail.com>
2020-07-04 10:33:10 +0300

    List of urls from s3 bucket for client testing

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import {getChartData, parse} from './models/log-parser';
 import VegaLiteChart from "./components/VegaLiteChart";
 import './App.css';
 
+const urls_list = [
+    "https://acp-cloud-logs.s3.eu-central-1.amazonaws.com/-acpcloud186ba.16420_cloud_aw180727--slr_benchmark",
+    "https://acp-cloud-logs.s3.eu-central-1.amazonaws.com/-acpcloud186bc.16420_ne190635_986878x1_cloud_benchmark_dup",
+    "https://acp-cloud-logs.s3.eu-central-1.amazonaws.com/-acpcloud186c2.16420_tan_wc78800_mrc200net2p_cloud%2B1",
+    "https://acp-cloud-logs.s3.eu-central-1.amazonaws.com/-acpcloud186d0.16420_cloud_a269321e.test2%2B2"
+]
+
 async function loadFile() {
-    let url = "https://gist.githubusercontent.com/alexbol99/79b5bea375006719b7b5aebbea2f47af/raw/e91b2f501e01abe28274d5715406c8f4499957d7/log.txt"
+    let url = urls_list[0]
     let text = await d3.text(url)
     let data = parse(text);
     return data;
