ca60c3802ea03c1217d0f728902ec02715684289
alexbol99 <alexbol99@gmail.com>
2020-07-08 17:55:43 +0300

    Updates

---
+++ precache-manifest.14482356c07c77e654aa58b53678495e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7181576cdb3728ee8a191b3cadd0802b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fa43bfa0661cc1541ba",
+    "url": "/static/css/main.5d3d637f.chunk.css"
+  },
+  {
+    "revision": "88e684d349d64f641381",
+    "url": "/static/js/2.792fb39b.chunk.js"
+  },
+  {
+    "revision": "e2a29c63a5962238e7b569ac4c5c4c44",
+    "url": "/static/js/2.792fb39b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fa43bfa0661cc1541ba",
+    "url": "/static/js/main.2b85f22d.chunk.js"
+  },
+  {
+    "revision": "6a683a63e5562042a90d",
+    "url": "/static/js/runtime-main.227962b0.js"
+  }
+]);
