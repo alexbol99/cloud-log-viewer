b31c79b51c627d2228f1ebdff1dc770260e93490
alexbol99 <alexbol99@gmail.com>
2020-11-15 20:12:48 +0200

    Updates

---
+++ precache-manifest.3c91fa05a7297f1bdb2d70934cd22d91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a7c2ee2f88e1695ed26a852b4a6a9ae",
+    "url": "/cloud-log-viewer/index.html"
+  },
+  {
+    "revision": "0907c359bc1376a44663",
+    "url": "/cloud-log-viewer/static/css/main.1ba391b9.chunk.css"
+  },
+  {
+    "revision": "5ab4957f3549fd48b2b4",
+    "url": "/cloud-log-viewer/static/js/2.ea990e48.chunk.js"
+  },
+  {
+    "revision": "6c193c15d307e6958283849ebead7c13",
+    "url": "/cloud-log-viewer/static/js/2.ea990e48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0907c359bc1376a44663",
+    "url": "/cloud-log-viewer/static/js/main.37df1b62.chunk.js"
+  },
+  {
+    "revision": "1d2445ab7c7e4acf8563",
+    "url": "/cloud-log-viewer/static/js/runtime-main.a4353b50.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cloud-log-viewer/precache-manifest.d6f6ed348aa297c53cc869554d09bb9f.js"
+  "/cloud-log-viewer/precache-manifest.3c91fa05a7297f1bdb2d70934cd22d91.js"
 );
 
 self.addEventListener('message', (event) => {
